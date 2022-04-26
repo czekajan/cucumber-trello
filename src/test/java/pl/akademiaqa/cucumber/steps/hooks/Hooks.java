df1f79a2950ea73695eb8c22541e4eee08bc476c
czeka <czekaj.an@gmail.com>
2022-04-26 13:21:14 +0200

    added hooks, cleanup after tests

---
+++ src/test/java/pl/akademiaqa/cucumber/steps/hooks/Hooks.java
@@ -0,0 +1,33 @@
+package pl.akademiaqa.cucumber.steps.hooks;
+
+import io.cucumber.java.After;
+import io.restassured.response.Response;
+import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
+import org.assertj.core.api.Assertions;
+import pl.akademiaqa.api.trello.board.DeleteBoardRequest;
+import pl.akademiaqa.handlers.api.RequestHandler;
+import pl.akademiaqa.handlers.shared.Context;
+import pl.akademiaqa.url.TrelloUrl;
+
+@RequiredArgsConstructor
+public class Hooks {
+
+    private final Context context;
+    private final RequestHandler requestHandler;
+    private final DeleteBoardRequest deleteBoardRequest;
+
+    @After(value = "@cleanup")
+    public void afterScenario(){
+
+        context.getBoards().values()
+                .forEach(boardId -> {
+                    requestHandler.setEndpoint(TrelloUrl.BOARDS);
+                    requestHandler.addPathParam("id", boardId);
+                    Response response = deleteBoardRequest.deleteBoard(requestHandler);
+                    Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+                });
+
+    }
+
+}
--- src/test/resources/pl.akademiaqa/create_new_board.feature
+++ src/test/resources/pl.akademiaqa/create_new_board.feature
@@ -1,7 +1,7 @@
 Feature: Create new board
 
+  @cleanup
   Scenario: I should be able to create new board with valid data
     Given I am authenticated to Trello
     When I create new board
     Then I can read created board details
-
