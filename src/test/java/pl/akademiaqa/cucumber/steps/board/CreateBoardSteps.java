12d1d7950635e58eec25802754d527a5fcaa4c67
czeka <czekaj.an@gmail.com>
2022-04-26 12:51:34 +0200

    added context and deleteBoardSteps

--- src/test/java/pl/akademiaqa/cucumber/steps/board/CreateBoardSteps.java
+++ src/test/java/pl/akademiaqa/cucumber/steps/board/CreateBoardSteps.java
@@ -9,6 +9,7 @@ import pl.akademiaqa.api.trello.board.CreateBoardRequest;
 import pl.akademiaqa.common.CommonValues;
 import pl.akademiaqa.handlers.api.RequestHandler;
 import pl.akademiaqa.handlers.api.ResponseHandler;
+import pl.akademiaqa.handlers.shared.Context;
 import pl.akademiaqa.url.TrelloUrl;
 
 @RequiredArgsConstructor
@@ -17,19 +18,26 @@ public class CreateBoardSteps {
     private final CreateBoardRequest createBoardRequest;
     private final RequestHandler requestHandler;
     private final ResponseHandler responseHandler;
+    private final Context context;
 
     @When("I create new board")
     public void i_create_new_board() {
+        createNewBoard();
+    }
 
+    @Given("The board already exist")
+    public void the_board_already_exist() {
+        createNewBoard();
+    }
+
+    private void createNewBoard() {
         requestHandler.setEndpoint(TrelloUrl.BOARDS);
         requestHandler.addQueryParam("name", CommonValues.BOARD_NAME);
 
         responseHandler.setResponse(createBoardRequest.createBoard(requestHandler));
         Assertions.assertThat(responseHandler.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
+
+        context.addBoard(CommonValues.BOARD_NAME, responseHandler.getId());
     }
 
-    @Given("The board already exist")
-    public void the_board_already_exist() {
-
-    }
 }
--- src/test/java/pl/akademiaqa/cucumber/steps/board/ReadBoardSteps.java
+++ src/test/java/pl/akademiaqa/cucumber/steps/board/ReadBoardSteps.java
@@ -9,6 +9,7 @@ import pl.akademiaqa.api.trello.board.ReadBoardRequest;
 import pl.akademiaqa.common.CommonValues;
 import pl.akademiaqa.handlers.api.RequestHandler;
 import pl.akademiaqa.handlers.api.ResponseHandler;
+import pl.akademiaqa.handlers.shared.Context;
 import pl.akademiaqa.url.TrelloUrl;
 
 @RequiredArgsConstructor
@@ -16,15 +17,13 @@ public class ReadBoardSteps {
 
     private final ReadBoardRequest readBoardRequest;
     private final RequestHandler requestHandler;
-    private final ResponseHandler responseHandler;
+    private final Context context;
 
     @Then("I can read created board details")
     public void i_can_read_created_board_details() {
 
-        requestHandler.setEndpoint(TrelloUrl.BOARDS);
-        requestHandler.addPathParam("id", responseHandler.getId());
+        Response response = readBoard();
 
-        Response response = readBoardRequest.readBoard(requestHandler);
         Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_OK);
         Assertions.assertThat(response.getBody().jsonPath().getString("name")).isEqualTo(CommonValues.BOARD_NAME);
     }
@@ -32,6 +31,19 @@ public class ReadBoardSteps {
     @Then("I should not see this board any more")
     public void i_should_not_see_this_board_any_more() {
 
+        Response response = readBoard();
+
+        Assertions.assertThat(response.getStatusCode()).isEqualTo(HttpStatus.SC_NOT_FOUND);
+    }
+
+    private Response readBoard() {
+        String boardId = context.getBoards().get(CommonValues.BOARD_NAME);
+
+        requestHandler.setEndpoint(TrelloUrl.BOARDS);
+        requestHandler.addPathParam("id", boardId);
+
+        Response response = readBoardRequest.readBoard(requestHandler);
+        return response;
     }
 
 
