dc020b8a704dc366a81ffb7d969e406cebcce5b2
czeka <czekaj.an@gmail.com>
2022-04-29 10:46:02 +0200

    added snippets to move_card_between_lists.feature

---
+++ src/test/java/pl/akademiaqa/cucumber/steps/card/CreateCardSteps.java
@@ -0,0 +1,12 @@
+package pl.akademiaqa.cucumber.steps.card;
+
+import io.cucumber.java.en.Given;
+
+public class CreateCardSteps {
+
+    @Given("I create car {string} on {string}")
+    public void i_create_car_on(String string, String string2) {
+
+    }
+
+}
---
+++ src/test/java/pl/akademiaqa/cucumber/steps/list/CreateNewListSteps.java
@@ -0,0 +1,13 @@
+package pl.akademiaqa.cucumber.steps.list;
+
+import io.cucumber.java.en.Given;
+
+public class CreateNewListSteps {
+
+    @Given("I create list {string} on {string}")
+    public void i_create_list_on(String string, String string2) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
